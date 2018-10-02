b45e0ff08bff98627b152e36ecc5050a43f9b2da
steven-critelli <43042731+steven-critelli@users.noreply.github.com>
2018-10-02 18:41:45 -0400

    Mistake made, reverting changes to laptop merge

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 //TODO:
-/*set function for setting priority color
-*if not-important == default
-*if important == orange
-*if very-important == red
+/*set function for setting priority color xxx
+*if not-important == default xxx
+*if important == orange xxx
+*if very-important == red xxx
 *
 *Add click and drag to reorder
 *
 *sublists -- i.e. clean bathroom (click, submenu) --> wipe down counters, clean tub
 *
-*Set colored bubble to appear on priority selection, and reset to default on adding item
-*Set colored bubble of choice to appear on right side of created items
-*
-*Google calendar integration? (look into API)
-*
-*instead of hover transition, use hover pop-out (i.e. bordered shadows on each edge)
 */
 
 /******
@@ -41,7 +35,10 @@ for (i = 0; i < close.length; i++) {
 }
 
 //Creates a new list item after "Add" button is clicked
+var cNodes = document.getElementById("myUL").childNodes;
 function newElement() {
+    var priority = document.getElementById("priority");
+    var selectedValue = priority.options[priority.selectedIndex].value;
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var x = document.createTextNode(inputValue);
@@ -52,6 +49,20 @@ function newElement() {
         document.getElementById("myUL").appendChild(li);
     }
     document.getElementById("myInput").value = "";
+
+    if (selectedValue == "unselected") {
+        if (inputValue ==''){
+            return
+        } else {
+            alert("A priority has not been selected.");
+        }
+    } else if (selectedValue == "normal") {
+        cNodes[cNodes.length-1].style.backgroundColor = "rgb(79, 194, 13)";//green
+    } else if (selectedValue == "important") {
+        cNodes[cNodes.length-1].style.backgroundColor = "rgb(255, 123, 0)";//orange
+    } else if (selectedValue == "very-important") {
+        cNodes[cNodes.length-1].style.backgroundColor = "rgb(228, 14, 14)";//red
+    }
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
